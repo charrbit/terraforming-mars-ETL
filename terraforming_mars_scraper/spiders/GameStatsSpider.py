0c4f36db4b239d64129e282f1239806711f836b2
charrbit <40149235+charrbit@users.noreply.github.com>
2023-11-25 14:33:57 -0600

    feat: spider parses game winner and game length

--- terraforming_mars_scraper/spiders/GameStatsSpider.py
+++ terraforming_mars_scraper/spiders/GameStatsSpider.py
@@ -1,9 +1,4 @@
 import scrapy
-
-# Website element structure
-# .game_end_victory_points
-    # .table .game_end_table
-        # .player_translucent_bg_color_playerColor
             
 class GameStatsSpider(scrapy.Spider):
     name = "gamestats"
@@ -12,8 +7,21 @@ class GameStatsSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        # Winner
+        winner = response.css(".game-end-winer-announcement")
+        yield {
+            "winner": winner.xpath("span//text()").get(),
+        }
+
+        # Number of Generations (length of the game)
+        generationsPlayed = response.css(".game_end_victory_points")
+        yield {
+            "generations_played": generationsPlayed.xpath("h2//text()").getall()[1].strip(),
+        }
+
+        # Player
         # Exclude the table header
-        for player in response.css("table tr")[1:]:
+        for player in response.css(".game_end_table tr")[1:]:
             playerData = player.xpath("td//text()").getall()
             del(playerData[1]) # Remove empty string element
             yield {

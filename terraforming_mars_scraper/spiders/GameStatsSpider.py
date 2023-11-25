c24d90736da10bc8cb7f2406eb24ffcf94db89fe
charrbit <40149235+charrbit@users.noreply.github.com>
2023-11-25 13:18:27 -0600

    feat: spider parses each players game data

--- terraforming_mars_scraper/spiders/GameStatsSpider.py
+++ terraforming_mars_scraper/spiders/GameStatsSpider.py
@@ -12,5 +12,23 @@ class GameStatsSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        pass
+        # Exclude the table header
+        for player in response.css("table tr")[1:]:
+            playerData = player.xpath("td//text()").getall()
+            del(playerData[1]) # Remove empty string element
+            yield {
+                "name": playerData[0],
+                "corporation": playerData[1],
+                "terraformer_rating": playerData[2],
+                "milestone_points": playerData[3],
+                "award_points": playerData[4],
+                "greenery_points": playerData[5],
+                "city_points": playerData[6],
+                "victory_points": playerData[7],
+                "escape_velocity_penality": playerData[8],
+                "total": playerData[9],
+                "final_credits": playerData[10],
+                "playtime": playerData[11],
+                "actions_taken": playerData[12],
+            }
         

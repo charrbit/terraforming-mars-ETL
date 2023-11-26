4bee2756c7c603380adf5b02ab5e1ea8a9c8d335
charrbit <40149235+charrbit@users.noreply.github.com>
2023-11-25 19:02:22 -0600

    feat: game mode spider now parses all game settings

--- terraforming_mars_scraper/spiders/GameModeSpider.py
+++ terraforming_mars_scraper/spiders/GameModeSpider.py
@@ -15,15 +15,48 @@ class GameModeSpider(scrapy.Spider):
             "number_of_players": playerCategory.css("input[checked]").attrib["value"],
         }
 
-        # Expansions Used
-        expansionCategory = gameSettingCategories[1]
-        expansions = [expansion.attrib["name"] for expansion in expansionCategory.css("input[checked]")]
+        # Number of Starting Corporations
+        optionCategory = gameSettingCategories[3]
+        startingCorporations = optionCategory.xpath("label//input").attrib["value"]
         yield {
-            "expansions": expansions,
+            "starting_corporations": startingCorporations,
         }
 
         # Board
         boardCategory = gameSettingCategories[2]
         yield {
-            "board": boardCategory.css("input[checked]").attrib["value"],
+            "board": boardCategory.css("input[checked]").attrib["value"].title(),
+        }
+
+        # Expansions Used
+        expansionCategory = gameSettingCategories[1]
+        expansions = []
+        for expansion in expansionCategory.css("input[checked]"):
+            inputID = expansion.attrib["id"]
+            label = expansionCategory.css(f"label[for='{inputID}']")
+            expansions.append(label.xpath("span//text()").get())
+        yield {
+            "expansions": expansions,
+        }
+
+        # Special Options
+        optionCategory = gameSettingCategories[3]
+        options = []
+        for option in optionCategory.css("input[checked]"):
+            inputID = option.attrib["id"]
+            label = optionCategory.css(f"label[for='{inputID}']")
+            options.append(label.xpath("span//text()").get())
+        yield {
+            "options": options,
+        }
+
+        # Multiplayer Options
+        mpOptionCategory = gameSettingCategories[4]
+        options = []
+        for option in mpOptionCategory.css("input[checked]"):
+            inputID = option.attrib["id"]
+            label = mpOptionCategory.css(f"label[for='{inputID}']")
+            options.append(label.xpath("span//text()").get())
+        yield {
+            "multiplayer_options": options,
         }
